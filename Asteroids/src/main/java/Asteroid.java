9a4620c4e828ef21063dc3ad6274b6552d2e01e5
Miguel <hisrubio51@gmail.com>
2017-05-21 01:02:00 +0200

    giro conseguido

--- Asteroids/src/main/java/Asteroid.java
+++ Asteroids/src/main/java/Asteroid.java
@@ -1,18 +1,20 @@
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
 
-public class Asteroid extends Applet{
+public class Asteroid extends Applet implements Runnable{
 	Image imgNave;
-	
 	Image imgfondo;
+	Nave nave;
+	Fondo fondo;
 	
 	Image noParpadeo;
 	Graphics noseve;
-	Nave nave;
-	Fondo fondo;
+	Thread animacion;
+
+
+	
 	
 	public void init(){
 		imgNave=getImage(getDocumentBase(),"nave/0.png");
@@ -20,26 +22,36 @@ public class Asteroid extends Applet{
 		noParpadeo = createImage(Fondo.WDIMENSION, Fondo.HDIMENSION);
 		noseve = noParpadeo.getGraphics();
 		
-		
 		fondo=new Fondo(imgfondo);
-		nave=new Nave(Fondo.WDIMENSION/2, Fondo.HDIMENSION/2, imgNave);
+		nave=new Nave(imgNave);
 		
 	}
+	public void start(){
+    	animacion = new Thread(this);
+    	animacion.start(); 
+    }
 	public void paint(Graphics g){
-		noseve.setColor(Color.black);
+		noseve.setColor(Color.white);
     	noseve.fillRect(0, 0, Fondo.WDIMENSION, Fondo.HDIMENSION);
     	
     	fondo.dibujar(noseve, this);
-    	nave.dibujar(noseve, this);
-    	
     	g.drawImage(noParpadeo, 0, 0, this);
+    	nave.dibujar(g, this);
 	}
 	public void update(Graphics g){
 		paint(g);
 	}
+	public void run(){
+		while(true){
+			try {
+				Thread.sleep(100);
+	        }catch (InterruptedException ex) {
+	        	System.out.println(ex.getMessage());
+	        }
+			repaint();
+		}
+	}
 	
-	 
-	 
 	 
 	 
 	 
