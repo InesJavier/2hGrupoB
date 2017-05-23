d91675bdcbf17b6374950f4acd6b6460d59a5845
Miguel <hisrubio51@gmail.com>
2017-05-23 16:34:51 +0200

    disparo

--- Asteroids/src/main/java/Asteroid.java
+++ Asteroids/src/main/java/Asteroid.java
@@ -2,27 +2,37 @@ import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Event;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Asteroid extends Applet implements Runnable{
 	Image imgNave;
-	Image imgfondo;
+	Image imgFondo;
+	Image imgDisparo;
 	Nave nave;
 	Fondo fondo;
+	List<Disparo>disparos;
+
 	
 	Image noParpadeo;
 	Graphics noseve;
 	Thread animacion;
 	
-	
 	public void init(){
 		imgNave=getImage(getDocumentBase(),"nave/0.png");
-		imgfondo=getImage(getDocumentBase(),"fondo/0.png"); 
+		imgFondo=getImage(getDocumentBase(),"fondo/0.png"); 
+		imgDisparo=getImage(getDocumentBase(),"disparos/bala.png");
 		noParpadeo = createImage(Fondo.WDIMENSION, Fondo.HDIMENSION);
 		noseve = noParpadeo.getGraphics();
 		
-		fondo=new Fondo(imgfondo);
+		disparos = new ArrayList<Disparo>();
+		for(int i=0; i<0; i++)
+    		disparos.add(new Disparo(imgDisparo,(int)nave.getX()-nave.ANCHO/2,(int)nave.getY()-nave.ALTO/2,nave.theta));
+		
+		fondo=new Fondo(imgFondo);
 		nave=new Nave(imgNave);
 		
 	}
@@ -31,12 +41,13 @@ public class Asteroid extends Applet implements Runnable{
     	animacion.start(); 
     }
 	public void paint(Graphics g){
-		noseve.setColor(Color.white);
+		noseve.setColor(Color.black);
     	noseve.fillRect(0, 0, Fondo.WDIMENSION, Fondo.HDIMENSION);
     	
     	fondo.dibujar(noseve, this);
-		
-		g.drawImage(noParpadeo, 0, 0, this);
+    	for(int i=0; i<disparos.size(); i++)
+    		disparos.get(i).dibujar(noseve,this);
+    	g.drawImage(noParpadeo, 0, 0, this);
 		nave.dibujar(g, this);
 	}
 	public void update(Graphics g){
@@ -44,6 +55,12 @@ public class Asteroid extends Applet implements Runnable{
 	}
 	public void run(){
 		while(true){
+			for(int i=0; i<disparos.size(); i++){
+				disparos.get(i).actualizar();
+				if(disparos.get(i).x>=Fondo.WDIMENSION || disparos.get(i).x<=0 || disparos.get(i).y>=Fondo.HDIMENSION || disparos.get(i).x<=0){
+					disparos.remove(i);
+				}
+			}
 			try {
 				Thread.sleep(100);
 	        }catch (InterruptedException ex) {
@@ -67,6 +84,8 @@ public class Asteroid extends Applet implements Runnable{
     		repaint();
     	}
     	if(tecla == 32){	//espacio
+    		disparos.add(new Disparo(imgDisparo,(int)nave.getX()+nave.ANCHO/2,(int)nave.getY()+nave.ALTO/2,nave.theta));
+    		repaint();
     	}
     	return true;
     }	 
