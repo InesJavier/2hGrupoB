f2011441efd5f4826320fe5f8870da9f2d085c27
Miguel Rubio <miguelRubio@gmail.com>
2018-02-25 21:34:32 +0100

    models que hereda de basemodule(gorka,s)

--- cine/models/__init__.py
+++ cine/models/__init__.py
@@ -4,3 +4,4 @@ from . import pelicula
 from . import actor
 from . import pais
 from . import director
+from . import models
--- cine/models/models.py
+++ cine/models/models.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
-# class cine(models.Model):
-#     _name = 'cine.cine'
+class modelo(models.Model):
+    _name = 'cine.modelo'
+    _inherit = 'base.entidad'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
